5fdd02b7c802902a72c964f5ef922159f6580519
muhammad bilal <muhammadbilal@nxb.com.pk>
2022-02-18 20:15:09 +0500

    add authentication signin and signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { JwtModule } from '@nestjs/jwt';
+import { secret } from './utils/constants';
+import { join } from 'path/posix';
+import { User, UserSchema } from './model/user.schema';
+import { UserService } from './service/user.service';
+import { UserController } from './controller/user.controller';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/userauth'),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
---
+++ src/service/user.service.ts
@@ -0,0 +1,77 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from '../model/user.schema';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response, NextFunction } from 'express';
+
+interface UserRequest extends Request {
+  user: any;
+}
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  async signup(userDet: User, jwt: JwtService): Promise<any> {
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(userDet.password, salt);
+    const reqBody = {
+      username: userDet.username,
+      email: userDet.email,
+      password: hash,
+    };
+    const newUser = new this.userModel(reqBody);
+    console.log('User in register ', newUser);
+    newUser.save();
+    const payload = {
+      id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+    };
+    const { email, username, id, createdDate } = newUser;
+    const user = { email, username, id, createdDate };
+    return {
+      accessToken: jwt.sign(payload),
+      user: user,
+    };
+  }
+
+  async signin(userDet: User, jwt: JwtService): Promise<any> {
+    const foundUser = await this.userModel
+      .findOne({ email: userDet.email })
+      .exec();
+    if (foundUser) {
+      const { password } = foundUser;
+      if (bcrypt.compare(userDet.password, password)) {
+        const payload = {
+          id: foundUser._id,
+          username: foundUser.username,
+          email: foundUser.email,
+        };
+        const { email, username, id, createdDate } = foundUser;
+        const user = { email, username, id, createdDate };
+        console.log('User in login ', user);
+        return {
+          accessToken: jwt.sign(payload),
+          user: user,
+        };
+      }
+      return new HttpException(
+        'Incorrect username or password',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    return new HttpException(
+      'Incorrect username or password',
+      HttpStatus.UNAUTHORIZED,
+    );
+  }
+  async verifytoken(token: string, jwt: JwtService): Promise<any> {
+    console.log('token in service file verify function :', token);
+    console.log('decode ', jwt.decode(token));
+    return {
+      myToken: jwt.decode(token),
+    };
+  }
+}
