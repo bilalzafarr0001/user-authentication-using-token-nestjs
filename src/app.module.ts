2a384e55e9d60b8840374e887b281f52d4fc8f88
muhammad bilal <muhammadbilal@nxb.com.pk>
2022-02-21 13:43:38 +0500

    add env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { JwtModule } from '@nestjs/jwt';
 import { secret } from './utils/constants';
 import { join } from 'path/posix';
 import { User, UserSchema } from './model/user.schema';
 import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
+console.log(MONGO_URL);
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/userauth'),
+    MongooseModule.forRoot(MONGO_URL),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     JwtModule.register({
       secret,
       signOptions: { expiresIn: '2h' },
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
     }),
   ],
   controllers: [AppController, UserController],
